3b84edc8837b2dd85abb72518869ce4088e727f2
A.Divya <108621599+Akkimdivya@users.noreply.github.com>
2024-05-10 22:35:49 +0530

    Update Banner.jsx

--- job-portal-finder/src/components/Banner.jsx
+++ job-portal-finder/src/components/Banner.jsx
@@ -6,7 +6,7 @@ const Banner = ({query,handleInputChange}) => {
     <div className='max-w-screen-2xl container mx-auto xl:px-24 px-4 md:py-20 py-14'>
         <h1 className='text-5xl font-bold mb-3'>Find Your <span className='text-black'>new job</span> role</h1>
         <p className='text-lg text-black/70 mb-8'>Thousands of jobs are waiting for you.</p>
-
+ {/*
         <form>
             <div className='flex justify-start md:flex-row flex-col md:gap-0 gap-4'>
                 <div className='flex md:rounded-s-md rounded shadow-sm ring-1 ring-inset ring-gray-400
@@ -18,7 +18,7 @@ const Banner = ({query,handleInputChange}) => {
                      />
                     <FiSearch className='absolute mt-2.5 ml-2 text-primary-400'/> 
                 </div>
-                {/*
+               
                 <div className='flex md:rounded-s-none rounded shadow-sm ring-1 ring-inset ring-gray-400
                 focus-within:ring-2 focus-within:ring-inset focus-within-ring-primary-600 md:w-1/3 w-full'>
                     <input type="text" name="title" id='title' placeholder='Location' className='block flex-1 border bg-transparent
@@ -28,9 +28,10 @@ const Banner = ({query,handleInputChange}) => {
                     <FiMapPin className='absolute mt-2.5 ml-2 text-primary-400'/> 
                 </div>
                 <button type='button' className='bg-primary py-2 px-8 text-white md:rounded-s-none rounded'>Search</button>
-                */}
+              
             </div>
         </form>
+          */}
     </div>
   )
 }
